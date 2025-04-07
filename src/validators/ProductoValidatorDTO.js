08e7680b3410254a2e09102c7cb65182d35d92d9
Diego2006 <dp82368@gmail.com>
2025-04-06 21:17:03 -0500

    Actualice el  ProductoValidatorDTO.js

--- src/validators/ProductoValidatorDTO.js
+++ src/validators/ProductoValidatorDTO.js
@@ -1,24 +1,49 @@
 import Joi from "joi";
 
 // Validaciones
-const nombre = Joi.string().min(3).max(100).required();
-const precio = Joi.number().min(0).required();
-const descripcion = Joi.string().max(500);
-const tipo_producto_id = Joi.string().pattern(/^[0-9a-fA-F]{24}$/).required();
+const name = Joi.string().min(3).max(100).required();
+const category = Joi.string().allow(''); // Permite string vacío
+const price = Joi.string().required();
+const stock = Joi.string().allow(''); // Permite string vacío
+const description = Joi.string().max(500).allow(''); // Permite string vacío
+const color = Joi.string().allow(''); // Permite string vacío
+const height = Joi.string().allow(''); // Permite string vacío
+const width = Joi.string().allow(''); // Permite string vacío
+const depth = Joi.string().allow(''); // Permite string vacío
+const imageUrl = Joi.string().allow(''); // Permite string vacío
+const tipo_producto_id = Joi.string().pattern(/^[0-9a-fA-F]{24}$/).allow(''); // Permite string vacío
 
 // Schemas de validación para productos
 const createProductoSchema = Joi.object({
-    nombre,
-    precio,
-    descripcion,
-    tipo_producto_id,
+    name: name,
+    category: category,
+    price: price,
+    stock: stock,
+    description: description,
+    color: color,
+    dimensions: Joi.object({
+        height: height,
+        width: width,
+        depth: depth,
+    }),
+    imageUrl: imageUrl,
+    tipo_producto_id: tipo_producto_id,
 });
 
 const updateProductoSchema = Joi.object({
-    nombre,
-    precio,
-    descripcion,
-    tipo_producto_id,
+    name: name,
+    category: category,
+    price: price,
+    stock: stock,
+    description: description,
+    color: color,
+    dimensions: Joi.object({
+        height: height,
+        width: width,
+        depth: depth,
+    }),
+    imageUrl: imageUrl,
+    tipo_producto_id: tipo_producto_id,
 });
 
 const getProductoSchema = Joi.object({
